d43804812084ea1237580851c76524c15d863bf6
momen-alshouha <momenalshouha@outlook.com>
2024-02-04 22:43:55 +0300

    generate environments

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,11 +2,10 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../interfaces/user';
+import { environment } from '../../environments/environment';
 
 const API = {
   USERS : 'https://jsonplaceholder.typicode.com/users',
-  USER : 'https://jsonplaceholder.typicode.com/users/1',
-  
 }
 
 @Injectable({
@@ -17,11 +16,11 @@ export class UserService {
   constructor(private _http : HttpClient) { }
 
   getUsers() : Observable<User[]> {
-    return this._http.get<User[]>(API.USERS);
+    return this._http.get<User[]>(`${environment.BASE_URL}/users`);
   }
 
-  getUser() : Observable<User> {
-    return this._http.get<User>(API.USER);
+  getUser(id : number) : Observable<User> {
+    return this._http.get<User>(`${environment.BASE_URL}/${id}`);
   }
 
 }
