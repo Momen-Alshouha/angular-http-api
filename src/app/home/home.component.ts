79834e3b7c37eaeb51cae0f9e7de8e2728d15d63
momen-alshouha <momenalshouha@outlook.com>
2024-02-12 22:51:05 +0300

    string interpolation used for sections description

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  
+  sectionsDescription : string ;
+  
+  constructor(){
+    this.sectionsDescription = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur, deserunt! Porro omnis, sapiente molestiae reiciendis accusantium aspernatur quia voluptatem ea laborum neque, id, odio a delectus alias numquam repudiandae labore.';
+  }
 }
